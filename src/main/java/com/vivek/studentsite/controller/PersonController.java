bec735c584839583ba8270f69571d25cbcbb7933
Vivek Ojha <ojha.vivek@gmail.com>
2023-06-26 12:18:45 +0530

    Added delete functionality

--- src/main/java/com/vivek/studentsite/controller/PersonController.java
+++ src/main/java/com/vivek/studentsite/controller/PersonController.java
@@ -3,11 +3,10 @@ package com.vivek.studentsite.controller;
 import com.vivek.studentsite.model.Student;
 import com.vivek.studentsite.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/student")
@@ -15,8 +14,24 @@ public class PersonController {
 
     @Autowired
     private PersonRepository personRepository;
-    @GetMapping
+    @GetMapping("/")
     public List<Student> getAll() {
         return personRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Student getAll(@PathVariable int id) {
+        return personRepository.findById(id).get();
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public String delete(@PathVariable int id) {
+        try {
+            personRepository.deleteById(id);
+            return "Studend with id - "+id+" deleted.";
+        } catch (Exception ex){
+            ex.printStackTrace();
+        }
+        return "Error in deleting studend with id - "+id;
     }
 }
